894df56a3fefe5d539597e1b1ef51d374f894f02
newbackjava <hera@naver.com>
2025-09-26 15:20:27 +0900

    음성텍스트변환

--- huggingface/한국어음성텍스트/한국어음성텍스트.py
+++ huggingface/한국어음성텍스트/한국어음성텍스트.py
@@ -5,4 +5,4 @@ from transformers import pipeline
 
 asr = pipeline("automatic-speech-recognition", model="openai/whisper-small")
 # 로컬 wav/mp3 파일 경로(예: sample_ko.wav)
-print(asr("sample_ko.wav")["text"])
+print(asr("./risk-136788.mp3")["text"])
